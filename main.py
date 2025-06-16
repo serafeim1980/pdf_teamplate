d8f7e3f126ea07693ae6ec476d18eb067e224225
Serafim <seraoriginal21@mail.com>
2025-06-16 11:28:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF
+from fpdf.enums import XPos, YPos
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, text=row["Topic"], align="L",
+         new_x=XPos.LMARGIN, new_y=YPos.NEXT)
+    pdf.line(10, 21, 200, 21)
+
+
+pdf.output("output.pdf")
